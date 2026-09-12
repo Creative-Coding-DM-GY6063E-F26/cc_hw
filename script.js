ce59bfe6b93f2d0fd2064c9f91ab1a99c1a484d3
nefelih <itsme.nefeli@gmail.com>
2026-09-12 19:57:24 -0400

    new quotes added

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // change the quotes in this array. Be mindful of the quotation marks!
 // this is the only part of the file you need to edit!
 const quotes = [
-  { text: "the intersection of programming, design, and art", source: "Scott" },
-  { text: "a chance for code to be wrong", source: "Sara" },
-  { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
-  { text: "process over results", source: "Sam" },
-  { text: "form over function", source: "Sean" },
-  { text: "iterative and experimental", source: "Scooby" },
-  { text: "a community of practice", source: "Seth" },
-  { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
+  { text: "a term we use to describe the computer programming software and people that make expressive art and design through code.", source: "Maura Kelly" },
+  { text: "a new interdisciplinary art form that bridges the gap between technologists and artists.", source: "Ahmad Moussa" },
+  { text: "a discovery-based process consisting of exploration, iteration, and reflection, using code as a primary medium, towards a media artefact designed for an artistic context.", source: "Mark C. Mitchell and Oliver Bown" },
+  { text: "the practice of making art with code.", source: "Andrew Bryant" },
+  { text: "at its heart, creative coding is the practice of using computer programming as a medium for artistic and personal expression.", source: "Laura Valtere" },
+  { text: "the practice of combining programming techniques with artistic expression in order to create aesthetically pleasing and optimized interactive digital experiences.", source: "SMU, Meadows School of the Arts" },
+  { text: "an approach to programming computers in which the primary goal is to create an aesthetic or expressive experience.", source: "The College of New Jersey" },
+  { text: "a place where experience in the field of design, love of explorative creativity and skills as a programmer come together.", source: "Patrik Hübner" }
 ];
 // no need to edit anything below this line! 
 // if you have made an error, you can check your history to see what might have gone wrong
