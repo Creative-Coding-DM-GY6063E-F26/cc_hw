e3c1000ee6e1490464de2780b68caeac85df7f5e
nefelih <itsme.nefeli@gmail.com>
2026-09-12 21:06:45 -0400

    error at 57 ?

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ function setup() {
 function pickQuote() {
   // take a random number and use that to identify what quote to use
   current = quotes[floor(quotes.length)]; 
-  console.log(current.text)
-
 }
 
 function draw() {
@@ -56,5 +54,5 @@ function newQuote() {
 }
 
 function mousePressed() {
-  newQuote();
+  newQuote(); /
 }
